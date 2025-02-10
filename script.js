d26c7ba6238a1e3bf9d1800ae2f89290fe188527
Jubayer Shikder <validjubayer@gmail.com>
2025-02-10 21:03:42 +0600

    calculator done

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
-const input = document.getElementById("temp-input");
-const celToFar = document.getElementById("cel-far");
-const farToCel = document.getElementById("far-cel");
-const mySubmit = document.getElementById("mySubmit");
-let converted = document.getElementById("converted");
+const display = document.getElementById("display");
 
-mySubmit.onclick = () =>{
-    let convert;
-    let temp = Number(input.value);
-    if (isNaN(temp)) {
-        converted.textContent = `input a valid number`
-    } else {
-        if (celToFar.checked) {
-            convert = (temp * 9/5) + 32;
-            converted.textContent = `${convert.toFixed(1)}°F`
-        }
-        else if (farToCel.checked) {
-            convert = (temp - 32) * 5/9;
-            converted.textContent = `${convert.toFixed(1)}°C`
-        }
-        else {
-            converted.textContent = `Select a unit`
-        }
-    }
+function appendToDisplay(input) {
+    display.textContent += input;
 }
+function clearDisplay() {
+    display.textContent = "";
+}
+function result () {
+    if (display.textContent = "Error")
+    try {
+        display.textContent = eval(display.textContent);
+    }
+    catch(error){
+        display.textContent = "Error"
+    }
+
+}
+
+
